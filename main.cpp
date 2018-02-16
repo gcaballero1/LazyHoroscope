99a001be61f7560055415bef3f0ad1dbe9a8d2bf
gcaballero1 <gcaballero1@mail.csuchico.edu>
2018-02-15 21:52:28 -0800

    Determines your initails and fortune

--- main.cpp
+++ main.cpp
@@ -22,7 +22,29 @@ string name2;
   nickname+= ".";
 
   cout<<"Welcome, "<<nickname<<", here is your fortune...\n";
-  cout<<"Your lucky number is " <<name1.length()+name2.length()<<endl;
+  cout<<"Your lucky number is " <<name1.length()<<endl;
+
+  if (name1[0] =='a' ||name1[0] == 'A' ||name1[0] == 'e'
+  ||name1[0] == 'E' ||name1[0] == 'i' ||name1[0] == 'I'
+  ||name1[0] == 'o' ||name1[0] == 'O' ||name1[0] == 'u'
+  ||name1[0] == 'U')
+  {
+  cout<<"you are destined to be famous!\n";
+  }
+  else 
+  {
+  cout<<"you should keep a low profile.\n";
+  }
+
+  int len=name2.length();
+  char let = name2[len-1];
+  if (let == 'a' ||let == 'A' ||let == 'e' ||let == 'E' 
+  ||let == 'i' ||let == 'I' ||let == 'o' ||let == 'O'
+  ||let == 'u' ||let == 'U')
+  {
+  cout<<"you have already met your true love.\n";
+  }
+  cout<<"have a good day!"<<endl;
 
   return 0;
 }
